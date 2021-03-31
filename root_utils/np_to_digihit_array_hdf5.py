cc205ed1ddcb9b6eda178fee1d456745b28a1c68
Nick Prouse <nprouse@triumf.ca>
2021-03-30 18:07:12 -0700

    Update conversion to digihit h5 file to include empty events with no triggers or hits, to preserve event indices across different files

--- root_utils/np_to_digihit_array_hdf5.py
+++ root_utils/np_to_digihit_array_hdf5.py
@@ -5,7 +5,7 @@ import subprocess
 from datetime import datetime
 import argparse
 import h5py
-import root_utils.pos_utils as pu
+
 
 def get_args():
     parser = argparse.ArgumentParser(description='convert and merge .npz files to hdf5')
@@ -16,11 +16,12 @@ def get_args():
     args = parser.parse_args()
     return args
 
+
 if __name__ == '__main__':
     config = get_args()
     print("ouput file:", config.output_file)
     f = h5py.File(config.output_file, 'w')
-    
+
     script_path = os.path.dirname(os.path.abspath(__file__))
     git_status = subprocess.check_output(['git', '-C', script_path, 'status', '--porcelain', '--untracked-files=no']).decode()
     if git_status:
@@ -49,56 +50,56 @@ if __name__ == '__main__':
         total_rows += hit_triggers.shape[0]
         event_triggers = np.full(hit_triggers.shape[0], np.nan)
         for i, (times, types, hit_trigs) in enumerate(zip(trigger_times, trigger_types, hit_triggers)):
-            good_triggers = np.where(types==0)[0]
-            if len(good_triggers)==0:
+            good_triggers = np.where(types == 0)[0]
+            if len(good_triggers) == 0:
                 continue
             first_trigger = good_triggers[np.argmin(times[good_triggers])]
-            nhits = np.count_nonzero(hit_trigs==first_trigger)
+            nhits = np.count_nonzero(hit_trigs == first_trigger)
             total_hits += nhits
             if nhits >= min_hits:
                 event_triggers[i] = first_trigger
                 good_hits += nhits
                 good_rows += 1
         file_event_triggers[input_file] = event_triggers
-    
+
     print(len(config.input_files), "files with", total_rows, "events with ", total_hits, "hits")
     print(good_rows, "events with at least", min_hits, "hits for a total of", good_hits, "hits")
 
-    dset_labels=f.create_dataset("labels",
-                                 shape=(good_rows,),
-                                 dtype=np.int32)
-    dset_PATHS=f.create_dataset("root_files",
-                                shape=(good_rows,),
-                                dtype=h5py.special_dtype(vlen=str))
-    dset_IDX=f.create_dataset("event_ids",
-                              shape=(good_rows,),
-                              dtype=np.int32)
-    dset_hit_time=f.create_dataset("hit_time",
-                                 shape=(good_hits, ),
-                                 dtype=np.float32)
-    dset_hit_charge=f.create_dataset("hit_charge",
-                                 shape=(good_hits, ),
-                                 dtype=np.float32)
-    dset_hit_pmt=f.create_dataset("hit_pmt",
-                                  shape=(good_hits, ),
-                                  dtype=np.int32)
-    dset_event_hit_index=f.create_dataset("event_hits_index",
-                                          shape=(good_rows,),
-                                          dtype=np.int64) # int32 is too small to fit large indices
-    dset_energies=f.create_dataset("energies",
-                                   shape=(good_rows, 1),
+    dset_labels = f.create_dataset("labels",
+                                   shape=(total_rows,),
+                                   dtype=np.int32)
+    dset_PATHS = f.create_dataset("root_files",
+                                  shape=(total_rows,),
+                                  dtype=h5py.special_dtype(vlen=str))
+    dset_IDX = f.create_dataset("event_ids",
+                                shape=(total_rows,),
+                                dtype=np.int32)
+    dset_hit_time = f.create_dataset("hit_time",
+                                     shape=(good_hits, ),
+                                     dtype=np.float32)
+    dset_hit_charge = f.create_dataset("hit_charge",
+                                       shape=(good_hits, ),
+                                       dtype=np.float32)
+    dset_hit_pmt = f.create_dataset("hit_pmt",
+                                    shape=(good_hits, ),
+                                    dtype=np.int32)
+    dset_event_hit_index = f.create_dataset("event_hits_index",
+                                            shape=(total_rows,),
+                                            dtype=np.int64)  # int32 is too small to fit large indices
+    dset_energies = f.create_dataset("energies",
+                                     shape=(total_rows, 1),
+                                     dtype=np.float32)
+    dset_positions = f.create_dataset("positions",
+                                      shape=(total_rows, 1, 3),
+                                      dtype=np.float32)
+    dset_angles = f.create_dataset("angles",
+                                   shape=(total_rows, 2),
                                    dtype=np.float32)
-    dset_positions=f.create_dataset("positions",
-                                    shape=(good_rows, 1, 3),
-                                    dtype=np.float32)
-    dset_angles=f.create_dataset("angles",
-                                 shape=(good_rows, 2),
-                                 dtype=np.float32)
     dset_veto = f.create_dataset("veto",
-                                 shape=(good_rows,),
+                                 shape=(total_rows,),
                                  dtype=np.bool_)
     dset_veto2 = f.create_dataset("veto2",
-                                  shape=(good_rows,),
+                                  shape=(total_rows,),
                                   dtype=np.bool_)
 
     offset = 0
@@ -110,45 +111,44 @@ if __name__ == '__main__':
         print(input_file, flush=True)
         npz_file = np.load(input_file, allow_pickle=True)
         good_events = ~np.isnan(file_event_triggers[input_file])
-        event_triggers = file_event_triggers[input_file][good_events]
-        event_ids = npz_file['event_id'][good_events]
-        root_files = npz_file['root_file'][good_events]
-        pids = npz_file['pid'][good_events]
-        positions = npz_file['position'][good_events]
-        directions = npz_file['direction'][good_events]
-        energies = npz_file['energy'][good_events]
-        hit_times = npz_file['digi_hit_time'][good_events]
-        hit_charges = npz_file['digi_hit_charge'][good_events]
-        hit_pmts = npz_file['digi_hit_pmt'][good_events]
-        hit_triggers = npz_file['digi_hit_trigger'][good_events]
-        track_pid = npz_file['track_pid'][good_events]
-        track_energy = npz_file['track_energy'][good_events]
-        track_stop_position = npz_file['track_stop_position'][good_events]
-        track_start_position = npz_file['track_start_position'][good_events]
-
+        event_triggers = file_event_triggers[input_file]
+        event_ids = npz_file['event_id']
+        root_files = npz_file['root_file']
+        pids = npz_file['pid']
+        positions = npz_file['position']
+        directions = npz_file['direction']
+        energies = npz_file['energy']
+        hit_times = npz_file['digi_hit_time']
+        hit_charges = npz_file['digi_hit_charge']
+        hit_pmts = npz_file['digi_hit_pmt']
+        hit_triggers = npz_file['digi_hit_trigger']
+        track_pid = npz_file['track_pid']
+        track_energy = npz_file['track_energy']
+        track_stop_position = npz_file['track_stop_position']
+        track_start_position = npz_file['track_start_position']
 
         offset_next += event_ids.shape[0]
 
         dset_IDX[offset:offset_next] = event_ids
         dset_PATHS[offset:offset_next] = root_files
-        dset_energies[offset:offset_next,:] = energies.reshape(-1,1)
-        dset_positions[offset:offset_next,:,:] = positions.reshape(-1,1,3)
+        dset_energies[offset:offset_next, :] = energies.reshape(-1, 1)
+        dset_positions[offset:offset_next, :, :] = positions.reshape(-1, 1, 3)
 
         labels = np.full(pids.shape[0], -1)
-        for l, v in label_map.items():
-            labels[pids==l] = v
+        for k, v in label_map.items():
+            labels[pids == k] = v
         dset_labels[offset:offset_next] = labels
 
-        polars = np.arccos(directions[:,1])
-        azimuths = np.arctan2(directions[:,2], directions[:,0])
-        dset_angles[offset:offset_next,:] = np.hstack((polars.reshape(-1,1),azimuths.reshape(-1,1)))
+        polars = np.arccos(directions[:, 1])
+        azimuths = np.arctan2(directions[:, 2], directions[:, 0])
+        dset_angles[offset:offset_next, :] = np.hstack((polars.reshape(-1, 1), azimuths.reshape(-1, 1)))
 
-        for i, (pids, energies, starts, stops) in enumerate(zip(track_pid, track_energy,track_start_position, track_stop_position)):
+        for i, (pids, energies, starts, stops) in enumerate(zip(track_pid, track_energy, track_start_position, track_stop_position)):
             muons_above_threshold = (np.abs(pids) == 13) & (energies > 166)
             electrons_above_threshold = (np.abs(pids) == 11) & (energies > 2)
             gammas_above_threshold = (np.abs(pids) == 22) & (energies > 2)
             above_threshold = muons_above_threshold | electrons_above_threshold | gammas_above_threshold
-            outside_tank = (np.linalg.norm(stops[:,(0,2)], axis=1) > config.radius) | (np.abs(stops[:, 1]) > config.half_height)
+            outside_tank = (np.linalg.norm(stops[:, (0, 2)], axis=1) > config.radius) | (np.abs(stops[:, 1]) > config.half_height)
             dset_veto[offset+i] = np.any(above_threshold & outside_tank)
             end_energy_estimate = energies - np.linalg.norm(stops - starts, axis=1)*2
             muons_above_threshold = (np.abs(pids) == 13) & (end_energy_estimate > 166)
@@ -159,7 +159,7 @@ if __name__ == '__main__':
 
         for i, (trigs, times, charges, pmts) in enumerate(zip(hit_triggers, hit_times, hit_charges, hit_pmts)):
             dset_event_hit_index[offset+i] = hit_offset
-            hit_indices = np.where(trigs==event_triggers[i])[0]
+            hit_indices = np.where(trigs == event_triggers[i])[0]
             hit_offset_next += len(hit_indices)
             dset_hit_time[hit_offset:hit_offset_next] = times[hit_indices]
             dset_hit_charge[hit_offset:hit_offset_next] = charges[hit_indices]
