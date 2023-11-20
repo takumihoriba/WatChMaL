0571b3eb134a8666171a4c851d062814e6d11ec1
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2023-11-20 14:20:18 -0800

    Update h5_dataset.py

--- watchmal/dataset/h5_dataset.py
+++ watchmal/dataset/h5_dataset.py
@@ -117,12 +117,14 @@ class H5CommonDataset(Dataset, ABC):
         if not self.initialized:
             self.initialize()
 
+        print(self.positions[item].shape)
+
         data_dict = {
             "labels": self.labels[item].astype(np.int64),
             #"range": self.range[item].astype(np.float32),
             # "energies": self.energies[item],
             # "angles": self.angles[item],
-            "positions": np.squeeze(self.positions[item], axis=1),
+            "positions": np.squeeze(self.positions[item], axis=2),
             #np.expand_dims(self.positions[item], axis=1), #self.positions[item].unsqueeze(1), # pred or true?
             # "event_ids": self.event_ids[item],
             "root_files": self.root_files[item],
