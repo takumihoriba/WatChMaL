b471e35cedff987ec5b93f7a33957ddfcc283c45
Ashley Ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2023-12-13 12:44:38 -0800

    Update resnet.py

--- watchmal/model/resnet.py
+++ watchmal/model/resnet.py
@@ -86,6 +86,7 @@ class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_input_channels, num_output_channels, stride=3, kernelSize=3, zero_init_residual=False,
                  conv_pad_mode='zeros'):
+        print(num_input_channels)
 
         super(ResNet, self).__init__()
 
